784f3ca6b708edd13cab59c871649899d3f02fca
Nana Yaw Sasu Appiah-Miracle <nanasasu@MacBook-Air.local>
2025-06-21 22:06:57 -0600

    Remove .next build directory and update gitignore

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3x4/J8IgO5R+7JtAHhnjF2jdbmDsPTSxyGNJ008kAis=\"\n}"
--- .next/static/chunks/src_app_page_tsx_651623c0._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_9e873850._.js",
-  "static/chunks/node_modules_6a262635._.js"
-],
-    source: "dynamic"
-});
